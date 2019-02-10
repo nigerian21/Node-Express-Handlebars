079ab4c503294ab8412c4ac8a92ef99fff25b8bf
Efe <efeowolide@gmail.com>
2019-02-10 12:09:42 -0600

    small changes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -30,7 +30,7 @@ router.put("/api/burger/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  cat.update({
+  burger.update({
     sleepy: req.body.sleepy
   }, condition, function(result) {
     if (result.changedRows == 0) {
@@ -45,7 +45,7 @@ router.put("/api/burger/:id", function(req, res) {
 router.delete("/api/burger/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  cat.delete(condition, function(result) {
+  burger.delete(condition, function(result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
